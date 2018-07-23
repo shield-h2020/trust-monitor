d246f79e11d62277dbfe9ba476915f3008312c1b
Marco De Benedictis <marco.debenedictis@polito.it>
2018-07-23 11:46:08 +0200

    Updated Dashboard connector
    - RabbitMQ host/port/exchange/topic can be configured to
    send JSON notifications to the Dashboard

---
+++ connectors/dashboard_connector/dashboard_settings.py
@@ -0,0 +1,17 @@
+###############################################################################
+# Configuration file for the Dashboard connector, which implements a rabbitmq
+# client
+###############################################################################
+
+# Hostname/IP address of the dashboard RabbitMQ server
+DASHBOARD_HOSTNAME = '10.101.10.30'
+# Port of the dashboard RabbitMQ server
+DASHBOARD_PORT = '5672'
+# Exchange of the dashboard Rabbit MQ server
+DASHBOARD_EXCHANGE = 'shield-dashboard-exchange'
+# Topic of the dashboard Rabbit MQ server
+DASHBOARD_TOPIC = 'shield.dare.x'
+# Number of attempts to query the Rabbit MQ server
+DASHBOARD_ATTEMPTS = 3
+# Retry delay when re-querying the Rabbit MQ server
+DASHBOARD_RETRY_DELAY = 5

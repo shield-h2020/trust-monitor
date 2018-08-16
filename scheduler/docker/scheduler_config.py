b16e6c8dc809b222bc4710601d68b1ef655abb75
Marco De Benedictis <marco.debenedictis@polito.it>
2018-08-16 15:50:02 +0200

    Updated config
    - Added PA timeout
    - Updated documentation of config

--- scheduler/docker/scheduler_config.py
+++ scheduler/docker/scheduler_config.py
@@ -2,9 +2,13 @@
 # Configuration file for the TM periodic attestation scheduler
 ###############################################################################
 
-# Periodic attestation interval (in seconds)
+# Periodic attestation interval (in seconds). Set to 0 or negative integer to
+# disable
 PA_SEC_INTERVAL = 60
 
 # TM attestation URL (base URL should be `https://tm_django_app/` in a Docker
 # Compose environment)
 PA_URL = "https://tm_django_app/nfvi_attestation_info"
+
+# Periodic attestation request timeout (in seconds)
+PA_SEC_TIMEOUT = 3

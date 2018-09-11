6bba6b78824efb6375fec9752622d1de41175575
Marco De Benedictis <marco.debenedictis@polito.it>
2018-09-11 13:41:25 +0200

    Updated dashboard connector config
    - Setup the shield.notification.tm topic for sending
    JSON data on untrusted nodes

--- connectors/dashboard_connector/dashboard_settings.py
+++ connectors/dashboard_connector/dashboard_settings.py
@@ -4,13 +4,13 @@
 ###############################################################################
 
 # Hostname/IP address of the dashboard RabbitMQ server
-DASHBOARD_HOSTNAME = '10.101.10.30'
+DASHBOARD_HOSTNAME = '10.101.10.130'
 # Port of the dashboard RabbitMQ server
 DASHBOARD_PORT = '5672'
 # Exchange of the dashboard Rabbit MQ server
 DASHBOARD_EXCHANGE = 'shield-dashboard-exchange'
 # Topic of the dashboard Rabbit MQ server
-DASHBOARD_TOPIC = 'shield.dare.x'
+DASHBOARD_TOPIC = 'shield.notification.tm'
 # Number of attempts to query the Rabbit MQ server
 DASHBOARD_ATTEMPTS = 3
 # Retry delay when re-querying the Rabbit MQ server

c5e94a8df7ed36fbf11643ceb24cd458506a3efc
Marco De Benedictis <marco.debenedictis@polito.it>
2018-12-13 14:02:03 +0000

    Added API version selector in vNSFO connector config

--- connectors/vnsfo_connector/vnsfo_settings.py
+++ connectors/vnsfo_connector/vnsfo_settings.py
@@ -7,3 +7,9 @@ VNSFO_BASE_URL = 'https://10.101.10.100:8448'
 
 # Max timeout of vNSFO API calls
 VNSFO_TIMEOUT = 5
+
+# OSM-release selector for certain VNSFO API commands
+# Accepted values are '' for legacy API call,
+# 'r2/' for explicit OSM r2,
+# 'r4/' for explicit OSM r4
+OSM_RELEASE = ''
